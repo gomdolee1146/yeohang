7f48fe4e0a889fbbfdb43c83fc136692e95f00a8
김선화 <ssun134568@gmail.com>
2023-03-17 12:03:48 +0900

    #230317_데이터스키마 만들기 작업

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const path = require('path');
 const app = express()
 const port = 3000
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
 
 mongoose.connect(process.env.MONGO_DB);
 
@@ -14,40 +15,91 @@ db.on('error', function(err) {
   console.log('DBerror:', err);
 })
 
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'views')));
+app.use(bodyParser.json()); // 미들웨어 - json으로 데이터 분석.
+
+var postSchema = mongoose.Schema({
+  title: {type:String, required:true},
+  body: {type:String, required:true},
+  createdAt: {type:Date, default:Date.now},
+  updatedAt: Date  
+})
+var Post = mongoose.model('post', postSchema);
+
+app.get('/posts', function(req, res) {    // index
+  Post.find().exec()
+    .then(posts => {
+      return res.json({success:true, data:posts});  
+    })
+    .catch(err => {
+      return res.json({success:false, message:err})
+    })
+})
+
+app.post('/posts', function(req, res){    // create
+  Post.create(req.body.post)
+    .then(post => {
+      return res.json({success:true, data:post})
+    })
+    .catch(err => {
+      return res.json({success:false, message:err})
+    })
+})
+
+app.get('/posts/:id', function(req, res){    // show
+  Post.findById(req.param.id)
+    .then(post => {
+      return res.json({success:true, data:post})  // 이부분 좀 더 확인..!
+    })
+    .catch(err => {
+      return res.json({success:false, message:err})
+    })
+})
+
+app.put('/posts/:id', function(req, res){    // update
+  req.body.post.updatedAt = Date.now();
+  Post.findByIdAndUpdate(req.params.id, req.body.post)
+    .then(post => {
+      return res.json({success:true, message:post._id+" updated"})
+    })
+    .catch(err => {
+      return res.json({success:false, message:err});
+    })
+})
+
+app.delete("/posts/:id", function(req, res){
+  Post.findByIdAndRemove(req.params.id)
+    .then(post => {
+      return res.json({success:true, message:post._id+" deleted"})
+    })
+    .catch(err => {
+      return res.json({success:false, message:err});
+    })
+})
+
+
+
 var dataSchema = mongoose.Schema({
   name: String,
   count: Number
 });
 var Data = mongoose.model('data', dataSchema);
 
-// Data.findOne({name:'myData'}, (err, data) => { 
-//   if(err) return console.log('데이터 에러', err);
-//   if(!data){
-//     Data.create({name:'myData', count:0}, function(err, data){
-//       if(err) return console.log('데이터에러', err);
-//       console.log('counter initialized:', data);
-//     })
-//   }
-// })
+
 Data.findOne({name:'myData'}).exec()
   .then((data) => {
+    if (!data) {
+      data = Data.create({name: 'myData', count: 0});
+    }
     return console.log('counter initialized:', data);
   })
-  .then((err) => {
-    return console.log('데이터 에러', err);
-  })
-  .then((data) => {
-    if(!data){
-      Data.create({name:'myData', count:0})
-      return console.log('카운트 시작:', data);
+  .catch((err, data) => {
+    if (!data){
+      if(err) return console.log('데이터 에러', err);
     }
-  })
-  .catch((err) => {
     return console.log('데이터에러', err);
   })
-
-app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname, 'views')));
 
 var data = {count:0};   // 서버에 저장될 데이터
 
@@ -56,37 +108,6 @@ app.get('/', function (req, res) {
   data.count++;
   res.render('main', data);
 });
-
-// get 신호받으면 count를 0으로, data를 main 렌더
-app.get('/reset', function(req, res) {
-  data.count = 0;
-  res.render('main', data);
-});
-
-/** 
- *  /set/count루트에 get 신호받으면, count query 확인. data.count에 대입.
- *  쿼리 스트링으로 된 주소를 서버에서 받기.
- */
-app.get('/set/count', function(req, res) {
-  if (req.query.count) data.count=req.query.count;
-  res.render('main', data);
-});
-
-/**
- *  루트에 콜론이 오면, 주소줄을 변수로 선언. 
- *  parameter로 저장.
- *  위쪽 주소랑 대입하면
- *  >> /set/count?count=num으로 변경됨
- */
-app.get('/set/:num', function(req, res) {
-  data.count=req.params.num;
-  res.render('main', data);
-})
-
-
-app.get('/', (req, res) => {
-  res.render('Hello World!')
-})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
